ff89c690f88a653164a389736c72198b2a2626e7
mattmccreesh <mmccrees@stevens.edu>
2016-09-11 21:57:43 -0400

    Add files via upload

---
+++ PracticeOneMain.java
@@ -0,0 +1,17 @@
+package CodePracticeOne;
+/*I want this to be the driver/main class of the package
+ * Programs should involve objects, polymorphism, and inheritence
+ * Maybe put in an interface or abstract class
+ * I would like recursion to be used in at least one method
+ * An example would be a war simulation with class people, civilian, soldier, each rank, weapons, methods for deciding who will win a battle, etc. Each soldier can have stats like k/d (or kills if only one death allowed and no come back to life) It will be like a video game
+ */
+public class PracticeOneMain
+{
+    public static void main(String[] args)
+    {
+	for(int i = 0; i < 100; i++)
+	{
+	    System.out.println(i);
+	}
+    }
+}
